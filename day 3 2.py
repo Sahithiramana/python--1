4f5a78e7fe6ba9786b62a2f86300ceef49ea55ff
Sahithiramana <121482156+Sahithiramana@users.noreply.github.com>
2023-02-03 00:50:26 -0800

    Add files via upload

---
+++ day 3 2.py
@@ -0,0 +1,14 @@
+
+
+def comb(L):
+	
+	for i in range(4):
+		for j in range(4):
+			for k in range(4):
+				
+				
+				if (i!=j and j!=k and i!=k):
+					print(L[i], L[j], L[k])
+					
+
+comb([1,4,5,6])
